c80c111e968ed846500fbcec4b8af02a82d2f7b1
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-03-10 00:12:27 +0100

    Create function to class(3).py

---
+++ Chapter 01/07. OOP/function to class(3).py
@@ -0,0 +1,31 @@
+
+
+class Primenumber:
+
+    """
+        List of all prime numbers.
+
+        Attributes: fnum, snum, tnum
+    """
+
+    def __init__(self, fnum, snum):
+        self.first_number = fnum
+        self.second_number = snum
+        pass
+
+    def prime_numbers(self):
+        for num in range(self.first_number, self.second_number):
+            for i in range(self.first_number, num):
+                if num % i == 0:
+                    break 
+            else:
+                print(num)
+
+
+
+prime = Primenumber(2, 30)
+
+prime.first_number
+prime.second_number
+
+prime.prime_numbers()
