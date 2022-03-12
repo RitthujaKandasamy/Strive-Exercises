cc348cfb72d6049e5b05964ce53d9d6220420487
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-03-13 00:07:24 +0100

    modified

--- Chapter 01/Web scrap/In python.py
+++ Chapter 01/Web scrap/In python.py
@@ -5,6 +5,5 @@ from bs4 import BeautifulSoup
 
 URL = "https://forecast.weather.gov/MapClick.php?x=276&y=148&site=lox&zmx=&zmy=&map_x=276&map_y=148"
 page = requests.get(URL)
-print(page)
 
 soup = BeautifulSoup(page.content, "html.parser")
