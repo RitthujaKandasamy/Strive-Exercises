65831c2c0cc143dd737e3b1d9e07d4590bf869a1
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-04-19 23:04:59 +0200

    train completed

--- Chapter 02/07. Gradient Boosting and Encoding/data_handler.py
+++ Chapter 02/07. Gradient Boosting and Encoding/data_handler.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
@@ -10,16 +10,45 @@ hello = "<3"
 def get_data(pth):
 
     data = pd.read_csv(pth)
+    print(data[:3])
 
-    # splittind the data
-    x_train, x_test, y_train, y_test = train_test_split(data.values[:,:-1], data.values[:,-1], test_size=0.2, random_state = 0)
+    X, y = data.iloc[:, :-1], data.iloc[:, -1]
+    print(X[:3])
+    print(y[:3])
 
-    # OE the new split data
-    ct = ColumnTransformer( [('ordinal', OrdinalEncoder(handle_unknown= 'use_encoded_value', unknown_value = -1), [1,4,5] )] )
+    # split 
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+
+    # OE data, because we want good tree model
+    # handle_unknown is used for giving the value for data that is not in the training set, if in further it appear in dataset
+    # eg: we trained with some data, but in further we want to train same extract, for that we use it
+    # by unknown_value we put the value for that data
+
+    oe = OrdinalEncoder(handle_unknown= 'use_encoded_value', unknown_value = -1)
+    scalar = StandardScaler()
+
+
+
+    # columntransform is used to transform
+    # ordinal and standard are name given to them , we can give any name we like
+    ct = ColumnTransformer(('ordinal', oe, ['sex', 'smoker', 'region']), 
+                               ('standard', scalar, ['age', 'bmi']),
+                                    remainder = 'passthrough')
+
+
+
+    # fit dataset
+    data_full = ct.fit(data)
+    # [:5, :] used to print first 5 full dataset
+    print(data_full[:5, :])
+
 
     # fit and transform
     x_train = ct.fit_transform(x_train)
     x_test = ct.transform(x_test)
+    
 
     # creating randomforest
     Rf_clf = RandomForestRegressor(random_state=0)
@@ -42,6 +71,4 @@ def get_data(pth):
     # predict Gb
     pred_GB = GB_clf.predict(x_test)
 
-    return x_train, x_test, y_train, y_test, pred_rf, pred_ada, pred_GB
-
-get_data('C:\\Users\\ritth\\code\\Strive\\Strive-Exercises\\Chapter 02\\07. Gradient Boosting and Encoding\\insurance.csv')
+    return x_train, x_test, y_train, y_test
--- Chapter 02/07. Gradient Boosting and Encoding/train.py
+++ Chapter 02/07. Gradient Boosting and Encoding/train.py
@@ -1,17 +1,32 @@
-import data_handler as dh
+import dataset as dh
+from xgboost import XGBRegressor
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
 
 
-# split data
+
 x_train, x_test, y_train, y_test = dh.get_data("C:\\Users\\ritth\\code\\Strive\\Strive-Exercises\\Chapter 02\\07. Gradient Boosting and Encoding\\insurance.csv")
 
-print(x_train, y_train, x_test, y_test)
+
+# check shape 
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
 
 
-# check with RF, Ada, GB
-models = [RandomForestRegressor(), GradientBoostingRegressor(), AdaBoostRegressor()]
+rf = RandomForestRegressor()
+ada = AdaBoostRegressor()
+gb = GradientBoostingRegressor()
+xgb = XGBRegressor()
 
 
-for model in models:
-    print(cross_val_score(model, x_train, y_train).mean())
+# create RF, Ada, GB, XGB in one line
+models = [rf, ada, gb, xgb]
+
+
+# fit and evaluating the performance
+for train_model in models:
+    fit = train_model.fit(x_train, y_train)
+    score = train_model.score(x_test, y_test)
+    cv = cross_validate(train_model, x_train, y_train)
+
+    print('{} : \n Score = {}, \n Crossvalidation = {} \n'.format(fit, score, cv))
+    print('Mean train cross validation score {} \n'.format(cv['test_score'].mean()))
