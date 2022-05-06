7160d0d08001563f15daa825c0e0c74928373408
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-05-06 21:15:11 +0200

    images added

--- Chapter 02/15. TimeSeries/Temp/visualization.py
+++ Chapter 02/15. TimeSeries/Temp/visualization.py
@@ -2,9 +2,6 @@ from timeseries2 import data, x, y
 from new_features import new_data
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
-import pandas as pd
-from sklearn.feature_selection import SelectKBest, chi2
 
 
 
@@ -15,11 +12,15 @@ data.corr()['T (degC)'].abs().sort_values().plot.barh()
 #plt.show()
 
 
-
+# matrix new
 matrix = new_data.corr()
 sns.heatmap(matrix, annot=True, vmax=1, vmin=-1, center=0, cmap='vlag')
 #plt.show()
 
-matrix = data.corr()
-sns.heatmap(matrix, annot=True, vmax=1, vmin=-1, center=0, cmap='vlag')
-plt.show()
+
+
+for col_name in new_data.columns:
+        plt.figure()
+        plt.hist(new_data[col_name])
+        plt.title(col_name)
+        plt.show()
