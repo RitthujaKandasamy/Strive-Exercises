95cc13e9d5febbdd37fd6a1e6f25986e2efcb849
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-05-30 00:00:41 +0200

    Update model.py

--- Chapter 03/03. MLP Regression/model.py
+++ Chapter 03/03. MLP Regression/model.py
@@ -7,20 +7,21 @@ from collections import OrderedDict
 
 
 
-# constructe the neural network archiecture
-def NeuralNetwork(input_layer, hidden_layer):
+# construct the neural network archiecture
+def NeuralNetwork(input_size, hidden_sizes, output_size):
 
 	
-       mlpmodel = nn.Sequential(
-        nn.Linear(input_layer, hidden_layer[0]),  
-        nn.Linear(),
-        nn.Linear(hidden_layer[0], hidden_layer[1]),  
-        nn.ReLU(),
-        nn.Linear(hidden_layer[1], hidden_layer[2]),  
-        nn.ReLU(),
-        nn.Linear(hidden_layer[2], hidden_layer[3]), 
-		nn.Linear(),
-		nn.Linear(hidden_layer[3], 1)
+       mlpmodel = nn.Sequential(OrderedDict([
+          ('fc1',   nn.Linear(input_size, hidden_sizes[0])),
+          ('linear1', nn.Linear()),
+          ('fc2',   nn.Linear(hidden_sizes[0], hidden_sizes[1])),
+          ('relu1', nn.ReLU()),
+          ('fc3', nn.Linear(hidden_sizes[1], hidden_sizes[2])),
+		  ('relu2', nn.ReLU()),
+          ('fc4',   nn.Linear(hidden_sizes[2], hidden_sizes[3])),
+          ('linear2', nn.Linear()),
+          ('output', nn.Linear(hidden_sizes[3], output_size))
 
-    )
+		  ]))
+    
        return mlpmodel
