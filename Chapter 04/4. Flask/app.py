0e21cfec822e7496f159f77f844e8d2c6263ab18
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-07-07 15:32:34 +0200

    updates

---
+++ Chapter 04/4. Flask/app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+
+## create instance
+app = Flask(__name__)
+
+
+@app.route("/")  ## --> decorator
+def index():
+    return render_template("home.html")
+
+@app.route("/")  ## --> decorator
+def about():
+    return render_template("about.html")
+
+
+
+## to run your app
+if __name__ =="__main__":
+    app.run(debug=True)
