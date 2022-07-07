b1aeb3e39cc7fd09e712e85efe6f61b81877a8b8
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-07-07 18:04:11 +0200

    Update app.py

--- Chapter 04/4. Flask/app.py
+++ Chapter 04/4. Flask/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
-@app.route("/")  ## --> decorator
+@app.route("/about")  ## --> decorator
 def about():
     return render_template("about.html")
 
