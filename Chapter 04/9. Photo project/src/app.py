4b64869ffabc43a21a6950313b9e6a56ba26604e
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-07-14 13:24:44 +0200

    Update app.py

--- Chapter 04/9. Photo project/src/app.py
+++ Chapter 04/9. Photo project/src/app.py
@@ -1,25 +1,30 @@
 from flask import Flask
 from flask_restful import Api
-from routes.home.route import HomeRoute, HomeRouteWithId
+from routes.home.route import HomeRoute
 from utils.db import db
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-    api = Api(app)
 
-    db.init_app(app)                 ## initialize the database
-    db.create_all(app=app)           ## create tables  
-
-    api.add_resource(HomeRoute, '/')
-    api.add_resource(HomeRouteWithId, '/<string:id>')
-    return app
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+api = Api(app)
+
+db.init_app(app)                 ## initialize the database
+db.create_all(app=app)           ## create tables  
+
+api.add_resource(HomeRoute, '/')
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
 
 
 
