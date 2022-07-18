afb6ea43923a6a29821c601b97695a4768f85441
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-07-18 08:53:35 +0200

    Update user.py

--- Chapter 04/6. Data project/src/utils/models/user.py
+++ Chapter 04/6. Data project/src/utils/models/user.py
@@ -3,12 +3,12 @@ from datetime import datetime
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
-    password = db.column(db.String(20))
-    #date_created = db.column(db.DateTime, default=datetime.utcnow)
-
+    password = db.Column(db.String(60), nullable=False)
+    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+        return f"User('{self.username}', '{self.email}', '{self.date_posted}')"
