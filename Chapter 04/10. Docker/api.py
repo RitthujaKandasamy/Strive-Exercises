6837abb5a3696aaca53ccdc68ececcf8bb566106
RitthujaKandasamy <99767517+RitthujaKandasamy@users.noreply.github.com>
2022-07-19 14:10:53 +0200

    Create api.py

---
+++ Chapter 04/10. Docker/api.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import db
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class Beers(Resource):
+
+    def get(self):
+        return db.get_beers()
+
+
+class Name(Resource):
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("Origin", type=str)
+
+        args = parser.parse_args()
+        origin = args['Origin']
+
+        return db.get_origin(origin)
+
+
+api.add_resource(Beers, "/beers")
+api.add_resource(Name, "/name")
+
+
+if __name__ =="__main__":
+    app.run('0.0.0.0')
